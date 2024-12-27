b848f610f429a51cc0680dc00ba612c765f69ff4
Andy <andy.greenhaw@gmail.com>
2024-12-27 01:36:33 -0600

    cleanup

--- new-backend/index.js
+++ new-backend/index.js
@@ -1,32 +1,3 @@
-const express = require('express');
-const { Pool } = require('pg');
-const cors = require('cors')
-const app = express();
-const port = 3001;
+// index.js
 
-// PostgreSQL connection pool
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'mydatabase',
-  password: 'andy',
-  port: 5432,
-});
-
-// Enable CORS for all routes
-app.use(cors());
-
-// Endpoint to get users
-app.get('/api/users', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+require('./server');
